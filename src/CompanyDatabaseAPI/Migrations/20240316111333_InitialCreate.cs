3aa80aa74fb23a3d7a0bc4ccbe404f819ccb86a6
moose <moose@moose.com>
2024-03-16 05:14:04 -0600

    Add EF Core Migrations

---
+++ src/CompanyDatabaseAPI/Migrations/20240316111333_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyDatabaseAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<string>(type: "nchar(5)", fixedLength: true, maxLength: 5, nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    ContactName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    ContactTitle = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Region = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    PostalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: true),
+                    Fax = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "City",
+                table: "Customers",
+                column: "City");
+
+            migrationBuilder.CreateIndex(
+                name: "CompanyName",
+                table: "Customers",
+                column: "CompanyName");
+
+            migrationBuilder.CreateIndex(
+                name: "PostalCode",
+                table: "Customers",
+                column: "PostalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "Region",
+                table: "Customers",
+                column: "Region");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
